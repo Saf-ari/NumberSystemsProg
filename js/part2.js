7ed854ba6275e526239c78cf7d3eee01d3bb046f
Alex Saffari <asaffari1@hwemail.com>
2020-01-21 13:17:27 -0800

    added binary notation

--- js/part2.js
+++ js/part2.js
@@ -4,35 +4,80 @@ function ConversionPart2() {
     var number = Math.abs(parseInt(document.getElementById("2_SignedInt").value));
 
     var outputValue = "";
-    while (number != 0)
-    {
-      outputValue = number % 2 + outputValue;
-      number = Math.trunc(number / 2);
-    }
-
-
-    while (outputValue.length<32){
-      outputValue = ""+ outputValue;
-    }
-
-
-
-    
-
     var outputValueTwosComplement = "";
 
-    for (var i =0; i< outputValue.length; i++){
-      if (outputValue.charAt(i)==0){
-        outputValueTwosComplement += 1;
+    //make binary of decimal
+    var signedBinary;
+
+    var UnsignedInt = parseInt(document.getElementById("2_SignedInt").value);
+    var UnsignedIntBaseFrom = 10;
+    var UnsignedIntBaseTo = 2;
+    var int;
+    var length = UnsignedInt.toString().length;
+      if(UnsignedIntBaseFrom != 10)
+    {
+      int = 0;
+      for(var i = 0; i < length; i++){
+        int2=Math.trunc(UnsignedInt/(Math.pow(10, length)));
+        length--;
+        int = int*UnsignedIntBaseFrom+int2;
+        UnsignedInt=UnsignedInt-int2;
       }
-      else {
-        outputValueTwosComplement += 0;
+    }
+    else{
+      int=UnsignedInt;
+    }
+    var output="";
+    while(int!=0)
+    {
+      var next=int%UnsignedIntBaseTo;
+      if(next >= 10){
+        if(next == 10)
+          next="A";
+        else if(next == 11)
+          next="B";
+        else if(next == 12)
+          next="C";
+        else if(next == 13)
+          next="D";
+        else if(next == 14)
+          next="E";
+        else if(next == 15)
+          next="F";
+      }
+      else{
+        next=next.toString();
+      }
+      output=next+output.substring(0);
+      int=Math.trunc(int/UnsignedIntBaseTo);
+    }
+    signedBinary = output;
+
+
+
+
+
+
+
+    // part 2
+    for (var i = 0; i < signedBinary.length; i++){
+//      outputValueTwosComplement = "" + signedBinary.charAt(i) + outputValueTwosComplement;
+      if (signedBinary.charAt(i)== 0){
+        outputValueTwosComplement = "" + outputValueTwosComplement + "1";
+      }
+      else{
+        outputValueTwosComplement = "" + outputValueTwosComplement +"0";
       }
     }
 
 
 
 
-       FormatAndShowOutput([outputValue, outputValueTwosComplement, SignedDecimalInt], 2);
+
+
+
+
+
+       FormatAndShowOutput([signedBinary, outputValueTwosComplement, SignedDecimalInt], 2);
 
 }
